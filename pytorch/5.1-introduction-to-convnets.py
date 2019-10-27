11794d871e68f8f80486a07bf5137325b4ee1526
Changyu_Liu <liuchangyu1111@gmail.com>
2019-10-27 23:47:49 +0800

    add new utils

--- pytorch/5.1-introduction-to-convnets.py
+++ pytorch/5.1-introduction-to-convnets.py
@@ -17,16 +17,20 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.utils.data as data
+import torchsummary
 import torchvision
 import torchvision.transforms as transforms
+
+from utils.eval import accuracy
+from utils.misc import AverageMeter
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 transform = transforms.Compose(
-  [transforms.ToTensor(),
+  [transforms.RandomHorizontalFlip(),
+   transforms.ToTensor(),
    transforms.Normalize((0.5,), (0.5,))])
 
-torchvision.
 trainset = torchvision.datasets.MNIST(root='~/pytorch_datasets', train=True,
                                       download=True, transform=transform)
 trainloader = data.DataLoader(trainset, batch_size=128,
@@ -37,94 +41,77 @@ testset = torchvision.datasets.MNIST(root='~/pytorch_datasets', train=False,
 testloader = data.DataLoader(testset, batch_size=128,
                              shuffle=False, num_workers=2)
 
-print(f"Pytorch Version: {torch.__version__}")
-
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+print(f"PyTorch Version: {torch.__version__}")
 
 
-# Let's display the architecture of our convnet so far:
+class Net(nn.Module):
+  def __init__(self):
+    super(Net, self).__init__()
+    self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+    self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+    self.fc1 = nn.Linear(5 * 5 * 64, 512)
+    self.fc2 = nn.Linear(512, 10)
 
-# In[3]:
-
-
-model.summary()
-
-
-# You can see above that the output of every `Conv2D` and `MaxPooling2D` layer is a 3D tensor of shape `(height, width, channels)`. The width 
-# and height dimensions tend to shrink as we go deeper in the network. The number of channels is controlled by the first argument passed to 
-# the `Conv2D` layers (e.g. 32 or 64).
-# 
-# The next step would be to feed our last output tensor (of shape `(3, 3, 64)`) into a densely-connected classifier network like those you are 
-# already familiar with: a stack of `Dense` layers. These classifiers process vectors, which are 1D, whereas our current output is a 3D tensor. 
-# So first, we will have to flatten our 3D outputs to 1D, and then add a few `Dense` layers on top:
-
-# In[4]:
+  def forward(self, x):
+    x = F.relu(F.max_pool2d(self.conv1(x), 2))
+    x = F.relu(F.max_pool2d(self.conv2(x), 2))
+    x = x.view(-1, 5 * 5 * 64)
+    x = self.fc1(x)
+    x = self.fc2(x)
+    return x
 
 
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10, activation='softmax'))
+net = nn.DataParallel(Net())
+net.to(device)
+torchsummary.summary(net, (1, 28, 28))
 
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
 
-# We are going to do 10-way classification, so we use a final layer with 10 outputs and a softmax activation. Now here's what our network 
-# looks like:
+# We are now ready to train our network, which in Keras is done via a call to the `fit` method of the network:
+# we "fit" the model to its training data.
+for epoch in range(5):  # loop over the dataset multiple times
+  losses = AverageMeter()
+  top1 = AverageMeter()
+  top5 = AverageMeter()
 
-# In[5]:
+  net.train()
+  for i, data in enumerate(trainloader, 0):
+    # get the inputs; data is a list of [inputs, labels]
+    inputs, labels = data[0].to(device), data[1].to(device)
 
+    # zero the parameter gradients
+    optimizer.zero_grad()
 
-model.summary()
+    # forward + backward + optimize
+    outputs = net(inputs)
+    loss = criterion(outputs, labels)
 
+    # measure accuracy and record loss
+    prec1, prec5 = accuracy(outputs, labels, topk=(1, 5))
+    losses.update(loss.item(), inputs.size(0))
+    top1.update(prec1, inputs.size(0))
+    top5.update(prec5, inputs.size(0))
 
-# As you can see, our `(3, 3, 64)` outputs were flattened into vectors of shape `(576,)`, before going through two `Dense` layers.
-# 
-# Now, let's train our convnet on the MNIST digits. We will reuse a lot of the code we have already covered in the MNIST example from Chapter 
-# 2.
+    loss.backward()
+    optimizer.step()
 
-# In[6]:
+    # print statistics
+    if i % 5 == 0:
+      print(f"Epoch [{epoch + 1}] [{i}/{len(trainloader)}]\t"
+            f"Loss {loss.item():.4f}\t"
+            f"Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t"
+            f"Prec@5 {top5.val:.3f} ({top5.avg:.3f})")
 
+net.eval()
+correct = 0
+total = 0
+with torch.no_grad():
+  for data in testloader:
+    images, labels = data[0].to(device), data[1].to(device)
+    outputs = net(images)
+    _, predicted = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (predicted == labels).sum().item()
 
-from keras.datasets import mnist
-from keras.utils import to_categorical
-
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
-
-train_images = train_images.reshape((60000, 28, 28, 1))
-train_images = train_images.astype('float32') / 255
-
-test_images = test_images.reshape((10000, 28, 28, 1))
-test_images = test_images.astype('float32') / 255
-
-train_labels = to_categorical(train_labels)
-test_labels = to_categorical(test_labels)
-
-
-# In[7]:
-
-
-model.compile(optimizer='rmsprop',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
-model.fit(train_images, train_labels, epochs=5, batch_size=64)
-
-
-# Let's evaluate the model on the test data:
-
-# In[8]:
-
-
-test_loss, test_acc = model.evaluate(test_images, test_labels)
-
-
-# In[9]:
-
-
-test_acc
-
-
-# While our densely-connected network from Chapter 2 had a test accuracy of 97.8%, our basic convnet has a test accuracy of 99.3%: we 
-# decreased our error rate by 68% (relative). Not bad! 
+print(f"Accuracy of the currect network on the 10000 test images: {100 * correct / total}%.")
